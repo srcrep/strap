cf2c8fbbb21156c6554495df8f855673eb683c7f
srcrep <106826286+srcrep@users.noreply.github.com>
2022-10-31 15:13:48 -0500

    Create config.py

---
+++ bet/config.py
@@ -0,0 +1,14 @@
+class amount:
+    def __init__(self):
+        self
+    bet =   input('Bet Amount: ')
+    increase = input('Percent increase on loss: ')
+    
+class xy:
+    def __init__(self):
+        self
+    auto = (x= , y= )
+    min = (x= ,y= )
+    bet1 = (x= , y= )
+    bet2 = (x= , y= )
+    
