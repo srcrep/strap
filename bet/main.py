1d281c22e01b303bb26580af90212b421ef43895
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-03 13:50:13 -0500

    Update main.py

--- bet/main.py
+++ bet/main.py
@@ -1,13 +1,54 @@
 import pyautogui
-import webbrowser
-import json
+import time
 
+def loop():
+    with pyautogui.hold('alt'):
+        pyautogui.press('p')
+    time.sleep(0.1)
+    pyautogui.write('surf bc.game/game/classic-dice')
+    time.sleep(0.1)
+    pyautogui.press('enter')
+    time.sleep(15)
+    with pyautogui.hold('ctrl'):
+        pyautogui.press('-', presses=5)
+    time.sleep(0.5)
+    pyautogui.doubleClick(x=296, y=372)
+    time.sleep(0.5)
+    pyautogui.write('4.95')
+    time.sleep(0.5)
+    pyautogui.click(x=173, y=123)
+    time.sleep(0.5)
+    pyautogui.doubleClick(x=98, y=165)
+    time.sleep(0.5)
+    pyautogui.write('0.0001')
+    time.sleep(0.5)
+    pyautogui.click(x=69, y=360)
+    time.sleep(0.5)
+    pyautogui.doubleClick(x=152, y=358)
+    time.sleep(0.5)
+    pyautogui.write('25')
+    time.sleep(0.5)
+    pyautogui.click(x=136, y=524)
+    return
 
-def config():
-    with open("config.json") as json_data_file:
-        data = json.load(json_data_file)
-    return data
-config = config()
+def speedmod():
+    pyautogui.click(x=374, y=496)
+    time.sleep(0.25)
+    pyautogui.click(x=405, y=496)
+    time.sleep(0.5)
+    return
 
-def bet():
+def main():
+    loop()
+    time.sleep(1)
+    speedmod()
+    time.sleep(1)
+    for _ in range(10):
+        loop()
+        return
+    return
+
+main()
+
+    
     
