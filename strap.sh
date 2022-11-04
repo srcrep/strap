0f90a93ad37539a652cc6f0bdd89bd27c72bd70c
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-03 19:51:56 -0700

    merge

--- strap.sh
+++ strap.sh
@@ -10,7 +10,6 @@ downloaddepends ()
   python3-pip \
   python3-tk \
   python3-dev \
-  git \
   --assume-yes \
   /
 }
@@ -29,21 +28,6 @@ setuprdp ()
   sudo systemctl restart xrdp
 }
 
-adduser ()
-{
-  sudo adduser user --gecos "First,Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-  echo "user:passwordincleartextcausefuckit" | sudo chpasswd
-}
-
-sign ()
-{
-  create a python program to execute to automate signing into bc.games
-  not a bad idea to go ahead and correct the sound and turbo mode bet here
-}
-
-bet ()
-{
-  python3 main.py
-}
-
-bet
+downloaddepends
+downloadreqs
+startrdp
