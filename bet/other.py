ee3ad3ed765d7f4221559a954380d9bb19fe585b
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-06 10:31:13 -0600

    merge

--- bet/other.py
+++ bet/other.py
@@ -12,6 +12,7 @@ def pause(a):
 
 def main():
 
+#first
     time.sleep(3)
     with pyautogui.hold('alt'):
         pyautogui.press('p')
@@ -23,28 +24,35 @@ def main():
     with pyautogui.hold('ctrl'):
         pyautogui.press('-')
     pause(120)
-    pyautogui.moveTo(x=232, y=372)
+    pyautogui.moveTo(x=523, y=582) #payout location
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('4.95')
+    pyautogui.write('4.95') #payout amount
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=139, y=117)
+    pyautogui.click(x=312, y=177) #turn on auto
     time.sleep(3)
-    pyautogui.click(x=60, y=358)
-    time.sleep(3)
-    pyautogui.moveTo(x=146, y=360)
+    pyautogui.moveTo(x=168, y=251) #input amount location
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('25')
+    pyautogui.write("0.001") #input amount
+    time.sleep(3)
+    pyautogui.click(x=145, y=560) #click "on-lose" to enable
+    time.sleep(3)
+    pyautogui.moveTo(x=261, y=563) #"on-lose" percentage increase location
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('25') #"on-lose" percentage increase amount
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=107, y=541)
+    pyautogui.click(x=243, y=770) #start
 
+#second
     time.sleep(3)
     with pyautogui.hold('alt'):
         pyautogui.press('p')
@@ -54,32 +62,39 @@ def main():
     pyautogui.press('enter')
     pause(120)
     with pyautogui.hold('ctrl'):
-        pyautogui.press('-', presses=6)
+        pyautogui.press('-')
     pause(120)
-    pyautogui.moveTo(x=232, y=372)
+    pyautogui.moveTo(x=523, y=582) #payout location
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('4.95')
+    pyautogui.write('4.95') #payout amount
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=345, y=375)
+    pyautogui.click(x=721, y=585)
     time.sleep(3)
-    pyautogui.click(x=139, y=117)
+    pyautogui.click(x=312, y=177) #turn on auto
     time.sleep(3)
-    pyautogui.click(x=60, y=358)
-    time.sleep(3)
-    pyautogui.moveTo(x=146, y=360)
+    pyautogui.moveTo(x=168, y=251) #input amount location
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('25')
+    pyautogui.write("0.001") #input amount
+    time.sleep(3)
+    pyautogui.click(x=145, y=560) #click "on-lose" to enable
+    time.sleep(3)
+    pyautogui.moveTo(x=261, y=563) #"on-lose" percentage increase location
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('25') #"on-lose" percentage increase amount
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=107, y=541)
+    pyautogui.click(x=243, y=770) #start
 
+#third
     time.sleep(3)
     with pyautogui.hold('alt'):
         pyautogui.press('p')
@@ -89,30 +104,37 @@ def main():
     pyautogui.press('enter')
     pause(120)
     with pyautogui.hold('ctrl'):
-        pyautogui.press('-', presses=6)
+        pyautogui.press('-')
     pause(120)
-    pyautogui.moveTo(x=232, y=372)
+    pyautogui.moveTo(x=523, y=582) #payout location
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('1.2375')
+    pyautogui.write('1.2375') #payout amount
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=139, y=117)
+    pyautogui.click(x=312, y=177) #turn on auto
     time.sleep(3)
-    pyautogui.click(x=60, y=358)
-    time.sleep(3)
-    pyautogui.moveTo(x=146, y=360)
+    pyautogui.moveTo(x=168, y=251) #input amount location
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('25')
+    pyautogui.write("0.001") #input amount
+    time.sleep(3)
+    pyautogui.click(x=145, y=560) #click "on-lose" to enable
+    time.sleep(3)
+    pyautogui.moveTo(x=261, y=563) #"on-lose" percentage increase location
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('25') #"on-lose" percentage increase amount
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=107, y=541)
+    pyautogui.click(x=243, y=770) #start
 
+#forth
     time.sleep(3)
     with pyautogui.hold('alt'):
         pyautogui.press('p')
@@ -122,30 +144,37 @@ def main():
     pyautogui.press('enter')
     pause(120)
     with pyautogui.hold('ctrl'):
-        pyautogui.press('-', presses=6)
+        pyautogui.press('-')
     pause(120)
-    pyautogui.moveTo(x=232, y=372)
+    pyautogui.moveTo(x=523, y=582) #payout location
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('1.2375')
+    pyautogui.write('1.2375') #payout amount
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=345, y=375)
+    pyautogui.click(x=721, y=585)
     time.sleep(3)
-    pyautogui.click(x=139, y=117)
+    pyautogui.click(x=312, y=177) #turn on auto
     time.sleep(3)
-    pyautogui.click(x=60, y=358)
-    time.sleep(3)
-    pyautogui.moveTo(x=146, y=360)
+    pyautogui.moveTo(x=168, y=251) #input amount location
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('25')
+    pyautogui.write("0.001") #input amount
+    time.sleep(3)
+    pyautogui.click(x=145, y=560) #click "on-lose" to enable
+    time.sleep(3)
+    pyautogui.moveTo(x=261, y=563) #"on-lose" percentage increase location
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('25') #"on-lose" percentage increase amount
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=107, y=541)
+    pyautogui.click(x=243, y=770) #start
+
     return
 main() 
