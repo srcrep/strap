e574b413e7ede9f27bb8142909681cf7b5f587f3
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-05 19:48:50 -0500

    Update setup.sh

--- run/setup.sh
+++ run/setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+sudo apt update -y &&
 sudo apt install dwm suckless-tools surf xrdp -y &&
 sudo apt install python3-pip -y &&
 sudo apt install python3-tk -y &&
 sudo apt install python3-dev -y &&
+sudo apt upgrade -y &&
 git clone https://github.com/srcrep/strap.git &&
 python3 -m pip install --upgrade pipreqs &&
 cd ~/strap/bet/ &&
 ~/.local/bin/pipreqs &&
 python3 -m pip install --upgrade -r requirements.txt &&
 cp ~/strap/bet/main.py ~/main.py &&
-echo "exec ~/main.py" >> ~/.bashrc
+sudo reboot
