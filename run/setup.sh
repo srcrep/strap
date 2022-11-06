cc6a323e28c9933469c9af79f6c4c166072a05c7
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-05 20:15:14 -0500

    Update setup.sh

--- run/setup.sh
+++ run/setup.sh
@@ -5,7 +5,6 @@ sudo apt install python3-pip -y &&
 sudo apt install python3-tk -y &&
 sudo apt install python3-dev -y &&
 sudo apt upgrade -y &&
-git clone https://github.com/srcrep/strap.git &&
 python3 -m pip install --upgrade pipreqs &&
 cd ~/strap/bet/ &&
 ~/.local/bin/pipreqs &&
