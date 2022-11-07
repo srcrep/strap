b9713c24a016c03d21acd6a76c0a9e149b15d503
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-06 20:09:35 -0600

    Update setup.sh

--- run/setup.sh
+++ run/setup.sh
@@ -9,5 +9,5 @@ python3 -m pip install --upgrade pipreqs &&
 cd ~/strap/bet/ &&
 ~/.local/bin/pipreqs &&
 python3 -m pip install --upgrade -r requirements.txt &&
-cp ~/strap/bet/main.py ~/main.py &&
+cp ~/strap/bet/other.py ~/main.py &&
 sudo reboot
