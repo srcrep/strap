bcb8a6b25b748869c6e26fa531dfb2835e6ac553
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-06 19:49:40 -0600

    Update other.py

--- bet/other.py
+++ bet/other.py
@@ -4,7 +4,7 @@ from time import sleep
 from tqdm import trange
 from os import system
 
-amt = float(input("Bet Amount: "))
+amt = '0.001'
 
 def pause(a):
     for i in trange(a):
@@ -13,8 +13,46 @@ def pause(a):
     return
 
 def main():
-
-#first
+    time.sleep(3)
+    with pyautogui.hold('alt'):
+        pyautogui.press('p')
+    time.sleep(3)
+    pyautogui.write('surf bc.game/game/classic-dice')
+    time.sleep(3)
+    pyautogui.press('enter')
+    pause(120)
+    with pyautogui.hold('ctrl'):
+        pyautogui.press('-', presses=6)
+    pause(120)
+    pyautogui.moveTo(x=232, y=372)
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('4.95')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=139, y=117)
+    time.sleep(3)
+    pyautogui.moveTo(x=80, y=165) #input amount location
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write(amt) #input amount
+    time.sleep(3)
+    pyautogui.click(x=60, y=358)
+    time.sleep(3)
+    pyautogui.moveTo(x=146, y=360)
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('25')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=107, y=541)
+"""
+#first lowest rdp res 800 x 600
     time.sleep(3)
     with pyautogui.hold('alt'):
         pyautogui.press('p')
@@ -24,7 +62,7 @@ def main():
     pyautogui.press('enter')
     pause(15)
     with pyautogui.hold('ctrl'):
-        pyautogui.press('-')
+        pyautogui.press('-', times=6)
     pause(15)
     pyautogui.moveTo(x=523, y=582) #payout location
     time.sleep(3)
@@ -53,7 +91,7 @@ def main():
     pyautogui.press('enter')
     time.sleep(3)
     pyautogui.click(x=243, y=770) #start
-"""
+
 #second
     time.sleep(3)
     with pyautogui.hold('alt'):
