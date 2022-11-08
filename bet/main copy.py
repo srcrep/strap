69d0280febcd5c3974c29bdd7df8902f96f1f52f
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-08 07:19:53 -0600

    merge

---
+++ bet/main copy.py
@@ -0,0 +1,176 @@
+import pyautogui
+import time
+from time import sleep
+from tqdm import trange
+from os import system
+
+amt = '0.001'
+
+def pause(a):
+    for i in trange(a):
+        sleep(1)
+        system('clear')
+    return
+
+def main():
+    #bcgame classic-dice
+    time.sleep(3)
+    with pyautogui.hold('alt'):
+        pyautogui.press('p')
+    time.sleep(3)
+    pyautogui.write('surf bc.game/game/classic-dice')
+    time.sleep(3)
+    pyautogui.press('enter')
+    pause(120)
+    with pyautogui.hold('ctrl'):
+        pyautogui.press('-', presses=6)
+    pause(120)
+    pyautogui.moveTo(x=232, y=372) # change payout
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('4.95')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=139, y=117) # click auto
+    time.sleep(3)
+    pyautogui.moveTo(x=80, y=165) #input amount location
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write(amt) #input amount
+    time.sleep(3)
+    pyautogui.click(x=60, y=358) # on auto
+    time.sleep(3)
+    pyautogui.moveTo(x=146, y=360) # on auto amount
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('25')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=107, y=541) # start
+
+    #bcgame limbo
+    time.sleep(3)
+    with pyautogui.hold('alt'):
+        pyautogui.press('p')
+    time.sleep(3)
+    pyautogui.write('surf bc.game/game/limbo')
+    time.sleep(3)
+    pyautogui.press('enter')
+    pause(120)
+    with pyautogui.hold('ctrl'):
+        pyautogui.press('-', presses=6)
+    pause(120)
+    pyautogui.moveTo(x=232, y=372) # change payout
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('4.95')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=139, y=117) # click auto
+    time.sleep(3)
+    pyautogui.moveTo(x=80, y=165) #input amount location
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write(amt) #input amount
+    time.sleep(3)
+    pyautogui.click(x=60, y=358) # on auto
+    time.sleep(3)
+    pyautogui.moveTo(x=146, y=360) # on auto amount
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('25')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=107, y=541) # start
+
+    #nanogames classic-dice
+    time.sleep(3)
+    with pyautogui.hold('alt'):
+        pyautogui.press('p')
+    time.sleep(3)
+    pyautogui.write('nanogames.io/classic-dice')
+    time.sleep(3)
+    pyautogui.press('enter')
+    pause(120)
+    with pyautogui.hold('ctrl'):
+        pyautogui.press('-', presses=6)
+    pause(120)
+    pyautogui.moveTo(x=232, y=372) # change payout
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('4.95')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=139, y=117) # click auto
+    time.sleep(3)
+    pyautogui.moveTo(x=80, y=165) #input amount location
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write(amt) #input amount
+    time.sleep(3)
+    pyautogui.click(x=60, y=358) # on auto
+    time.sleep(3)
+    pyautogui.moveTo(x=146, y=360) # on auto amount
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('25')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=107, y=541) # start
+    
+    #nanogames limbo
+    time.sleep(3)
+    with pyautogui.hold('alt'):
+        pyautogui.press('p')
+    time.sleep(3)
+    pyautogui.write('nanogames.io/limbo')
+    time.sleep(3)
+    pyautogui.press('enter')
+    pause(120)
+    with pyautogui.hold('ctrl'):
+        pyautogui.press('-', presses=6)
+    pause(120)
+    pyautogui.moveTo(x=232, y=372) # change payout
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('4.95')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=139, y=117) # click auto
+    time.sleep(3)
+    pyautogui.moveTo(x=80, y=165) #input amount location
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write(amt) #input amount
+    time.sleep(3)
+    pyautogui.click(x=60, y=358) # on auto
+    time.sleep(3)
+    pyautogui.moveTo(x=146, y=360) # on auto amount
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write('25')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.click(x=107, y=541) # start
+
+main()
--- run/su.sh
+++ run/su.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+function pause(){
+ read -s -n 1 -p "Press "Enter" to reboot now . . ."
+ echo ""
+}
 apt install xrdp -y &&
 adduser xrdp ssl-cert &&
 systemctl restart xrdp &&
-adduser user &&
-usermod -aG sudo user &&
-exit
+pause
+reboot
