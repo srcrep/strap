a9237525d304df826e2a89cefccbafdcdd0ea71f
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-08 13:46:17 -0600

    Update setup.sh

--- run/setup.sh
+++ run/setup.sh
@@ -4,10 +4,11 @@ function pause(){
  echo ""
 }
 sudo apt update -y &&
-sudo apt install dwm suckless-tools surf -y &&
-sudo apt install python3-pip -y &&
-sudo apt install python3-tk -y &&
-sudo apt install python3-dev -y &&
+sudo apt install dwm suckless-tools surf xrdp -y &&
+# sudo apt install python3-pip -y &&
+# sudo apt install python3-tk -y &&
+# sudo apt install python3-dev -y &&
 sudo apt upgrade -y &&
 pause &&
-sudo su
+sudo su &&
+bash strap/run/su.sh
