a8db35380a141660b2d5d40e56ac49638e558930
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-08 08:29:14 -0600

    merge

--- bet/.main.py
+++ bet/.main.py
@@ -1,10 +1,21 @@
+#!/bin/python3
+
 import pyautogui
 import time
 from time import sleep
 from tqdm import trange
 from os import system
+from config import Config as config
 
-amt = '0.001'
+payout_location = config.payout_location
+payout_amount = config.payout_amount
+auto_bet_location = config.auto_bet_location
+bet_amount_location = config.bet_amount_location
+bet_amount = config.bet_amount
+enable_loss_increase_location = config.enable_loss_increase_location
+loss_increase_location = config.loss_increase_location
+loss_increase_percentage = config.loss_increase_percentage
+start_location = config.start_location
 
 def pause(a):
     for i in trange(a):
@@ -25,152 +36,31 @@ def main():
     with pyautogui.hold('ctrl'):
         pyautogui.press('-', presses=6)
     pause(120)
-    pyautogui.moveTo(x=232, y=372) # change payout
+    pyautogui.moveTo(change_payout_location) # change payout
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('4.95')
+    pyautogui.write(payout_amount)
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=139, y=117) # click auto
+    pyautogui.click(auto_bet_location) # click auto
     time.sleep(3)
-    pyautogui.moveTo(x=80, y=165) #input amount location
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write(amt) #input amount
-    time.sleep(3)
-    pyautogui.click(x=60, y=358) # on auto
-    time.sleep(3)
-    pyautogui.moveTo(x=146, y=360) # on auto amount
+    pyautogui.moveTo(bet_amount_location) #input amount location
     time.sleep(3)
     pyautogui.doubleClick()
     time.sleep(3)
-    pyautogui.write('25')
+    pyautogui.write(bet_amount) #input amount
+    time.sleep(3)
+    pyautogui.click(enable_loss_increase_location) # on auto
+    time.sleep(3)
+    pyautogui.moveTo(loss_increase_location) # on auto amount
+    time.sleep(3)
+    pyautogui.doubleClick()
+    time.sleep(3)
+    pyautogui.write(loss_increase_percentage)
     time.sleep(3)
     pyautogui.press('enter')
     time.sleep(3)
-    pyautogui.click(x=107, y=541) # start
-
-    #bcgame limbo NEED TO VERIFY LACATIONS ARE SAME AS CLASSIC DICE
-    time.sleep(3)
-    with pyautogui.hold('alt'):
-        pyautogui.press('p')
-    time.sleep(3)
-    pyautogui.write('surf bc.game/game/limbo')
-    time.sleep(3)
-    pyautogui.press('enter')
-    pause(120)
-    with pyautogui.hold('ctrl'):
-        pyautogui.press('-', presses=6)
-    pause(120)
-    pyautogui.moveTo(x=232, y=372) # change payout
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write('4.95')
-    time.sleep(3)
-    pyautogui.press('enter')
-    time.sleep(3)
-    pyautogui.click(x=139, y=117) # click auto
-    time.sleep(3)
-    pyautogui.moveTo(x=80, y=165) #input amount location
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write(amt) #input amount
-    time.sleep(3)
-    pyautogui.click(x=60, y=358) # on auto
-    time.sleep(3)
-    pyautogui.moveTo(x=146, y=360) # on auto amount
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write('25')
-    time.sleep(3)
-    pyautogui.press('enter')
-    time.sleep(3)
-    pyautogui.click(x=107, y=541) # start
-
-    #nanogames classic-dice NEED TO VERIFY LOCATIONS ARE THE SAME AS BCGAME
-    time.sleep(3)
-    with pyautogui.hold('alt'):
-        pyautogui.press('p')
-    time.sleep(3)
-    pyautogui.write('nanogames.io/classic-dice')
-    time.sleep(3)
-    pyautogui.press('enter')
-    pause(120)
-    with pyautogui.hold('ctrl'):
-        pyautogui.press('-', presses=6)
-    pause(120)
-    pyautogui.moveTo(x=232, y=372) # change payout
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write('4.95')
-    time.sleep(3)
-    pyautogui.press('enter')
-    time.sleep(3)
-    pyautogui.click(x=139, y=117) # click auto
-    time.sleep(3)
-    pyautogui.moveTo(x=80, y=165) #input amount location
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write(amt) #input amount
-    time.sleep(3)
-    pyautogui.click(x=60, y=358) # on auto
-    time.sleep(3)
-    pyautogui.moveTo(x=146, y=360) # on auto amount
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write('25')
-    time.sleep(3)
-    pyautogui.press('enter')
-    time.sleep(3)
-    pyautogui.click(x=107, y=541) # start
-    
-    #nanogames limbo
-    time.sleep(3)
-    with pyautogui.hold('alt'):
-        pyautogui.press('p')
-    time.sleep(3)
-    pyautogui.write('nanogames.io/limbo')
-    time.sleep(3)
-    pyautogui.press('enter')
-    pause(120)
-    with pyautogui.hold('ctrl'):
-        pyautogui.press('-', presses=6)
-    pause(120)
-    pyautogui.moveTo(x=232, y=372) # change payout
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write('4.95')
-    time.sleep(3)
-    pyautogui.press('enter')
-    time.sleep(3)
-    pyautogui.click(x=139, y=117) # click auto
-    time.sleep(3)
-    pyautogui.moveTo(x=80, y=165) #input amount location
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write(amt) #input amount
-    time.sleep(3)
-    pyautogui.click(x=60, y=358) # on auto
-    time.sleep(3)
-    pyautogui.moveTo(x=146, y=360) # on auto amount
-    time.sleep(3)
-    pyautogui.doubleClick()
-    time.sleep(3)
-    pyautogui.write('25')
-    time.sleep(3)
-    pyautogui.press('enter')
-    time.sleep(3)
-    pyautogui.click(x=107, y=541) # start
-
+    pyautogui.click(start_location) # start
 main()
---
+++ bet/src/config.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+class Config: #800x600 currently... need to decide best way to take different resolutions
+    def __init__(self):
+        self
+        self.payout_location = ('232','372')
+        self.payout_amount = '4.95'
+        self.auto_bet_location = ('139','117')
+        self.bet_amount_location = ('80','165')
+        self.bet_amount = '0.0001'
+        self.enable_loss_increase_location = ('60','358')
+        self.loss_increase_location = ('146','360')
+        self.loss_increase_percentage = '25'
+        self.start_location = ('107','541')
+        return payout_location, payout_amount, auto_bet_location, bet_amount_location. bet_amount, enable_loss_increase_location, loss_increase_location, loss_increase_percentage, start_location
+    return payout_location, payout_amount, auto_bet_location, bet_amount_location. bet_amount, enable_loss_increase_location, loss_increase_location, loss_increase_percentage, start_location
