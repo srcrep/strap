41334cfa97bdade75e0ce65dcb4ee5af3a1c673e
srcrep <106826286+srcrep@users.noreply.github.com>
2022-11-08 08:04:17 -0600

    merge

--- run/reqs.sh
+++ run/reqs.sh
@@ -3,5 +3,5 @@ python3 -m pip install --upgrade pipreqs &&
 cd ~/strap/bet/ &&
 ~/.local/bin/pipreqs &&
 python3 -m pip install --upgrade -r requirements.txt &&
-cp ~/strap/bet/other.py ~/main.py &&
+cp ~/strap/bet/.main.py ~/.main.py &&
 sudo reboot
