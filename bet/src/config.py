4468bfa80d4f5e58972e3b0b182d1946315af0b2
srcrep <106826286+srcrep@users.noreply.github.com>
2022-12-11 20:30:05 -0600

    merge

--- bet/src/config.py
+++ bet/src/config.py
@@ -7,79 +7,177 @@ bet_amount = float(input('Bet Amount : '))
 loss_increase_percentage = int(input('Percentage Increase on Loss : '))
              
 class Config: #800x600 currently... need to decide best way to take different resolutions
+
     def __init__(self, screen_width, screen_height):
         self
-        self.payout_location = payout_location(screen_height, screen_width)
-        self.payout_amount = payout_amount
-        self.auto_bet_location = auto_bet_location(screen_height, screen_width)
-        self.bet_amount_location = bet_amount_location(screen_height, screen_width)
-        self.bet_amount = bet_amount
-        self.enable_loss_increase_location = enable_loss_increase_location(screen_height, screen_width)
-        self.loss_increase_location = loss_increase_location(screen_height, screen_width)
-        self.loss_increase_percentage = loss_increase_percentage
-        self.start_location = start_location(screen_height, screen_width)
-        
-    def auto_location(screen_width, screen_height):
-        if screen_width == 800:
-            if screen_height == 600:
-                x = 196
-                y = 151
+        self.classic_dice(screen_height, screen_width) = classic_dice(screen_height, screen_width)
+        self.limbo(screen_height, screen_width) = limbo(screen_height, screen_width)
+        self.ultimate_dice(screen_height, screen_width) = ultimate_dice(screen_height, screen_width)
+
+    def classic_dice(screen_height, screen_width):
+
+        def auto_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 197
+                    y = 151
+                    return x, y
                 return x, y
             return x, y
-        return x, y
-    def payout_location(screen_width, screen_height):
-        if screen_width == 800:
-            if screen_height == 600:
-                x = 275
-                y = 311
+
+        def payout_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 277
+                    y = 311
+                    return x, y
                 return x, y
             return x, y
-        return x, y
-    def bet_amount_location(screen_width, screen_height):
-        if screen_width == 800:
-            if screen_height == 600:
-                x = 145
-                y = 179
+
+        def bet_amount_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 150
+                    y = 177
+                    return x, y
                 return x, y
             return x, y
-        return x, y
-    def enable_loss_increase_location(screen_width, screen_height):
-        if screen_width == 800:
-            if screen_height == 600:
-                x = 147
-                y = 289
+
+        def enable_loss_increase_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 139
+                    y = 294
+                    return x, y
                 return x, y
             return x, y
-        return x, y
-    def loss_increase_location(screen_width, screen_height):
-        if screen_width == 800:
-            if screen_height == 600:
-                x = 187
-                y = 289
+
+        def loss_increase_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 178
+                    y = 292
+                    return x, y
                 return x, y
             return x, y
-        return x, y
-    def ud_enable_loss_increase_location(screen_width, screen_height):
-        if screen_width == 800:
-            if screen_height == 600:
-                x = 147
-                y = 265
+
+        def start_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 172
+                    y = 372
+                    return x, y
                 return x, y
             return x, y
-        return x, y
-    def ud_loss_increase_location(screen_width, screen_height):
-        if screen_width == 800:
-            if screen_height == 600:
-                x = 187
-                y = 265
+
+    def limbo(screen_height, screen_width):
+
+        def auto_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 196
+                    y = 151
+                    return x, y
                 return x, y
             return x, y
-        return x, y
-    def start_location(screen_width, screen_height):
-        if screen_width == 800:
-            if screen_height == 600:
-                x = 176
-                y = 371
+
+        def payout_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 269
+                    y = 328
+                    return x, y
                 return x, y
             return x, y
-        return x, y
+
+        def bet_amount_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 145
+                    y = 178
+                    return x, y
+                return x, y
+            return x, y
+
+        def enable_loss_increase_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 141
+                    y = 294
+                    return x, y
+                return x, y
+            return x, y
+
+        def loss_increase_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 178
+                    y = 294
+                    return x, y
+                return x, y
+            return x, y
+
+        def start_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 170
+                    y = 373
+                    return x, y
+                return x, y
+            return x, y
+
+    def ultimate_dice(screen_height, screen_width):
+
+        def auto_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 198
+                    y = 151
+                    return x, y
+                return x, y
+            return x, y
+
+        def payout_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 271
+                    y = 332
+                    return x, y
+                return x, y
+            return x, y
+
+        def bet_amount_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 149
+                    y = 178
+                    return x, y
+                return x, y
+            return x, y
+
+        def enable_loss_increase_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 146
+                    y = 266
+                    return x, y
+                return x, y
+            return x, y
+
+        def loss_increase_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 186
+                    y = 266
+                    return x, y
+                return x, y
+            return x, y
+
+        def start_location(screen_width, screen_height):
+            if screen_width == 800:
+                if screen_height == 600:
+                    x = 168
+                    y = 372
+                    return x, y
+                return x, y
+            return x, y
